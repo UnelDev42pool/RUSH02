abb969beb3bc43bd51883be12f3c595a5f32123d
UnelDev, Enee Di-iorio <enee.prog@gmail.com>
2023-07-22 13:10:58 +0200

    change name of type dictionary

--- ex00/ft.h
+++ ex00/ft.h
@@ -6,18 +6,18 @@
 /*   By: edi-iori <edi-iori@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/22 09:22:51 by deydoux           #+#    #+#             */
-/*   Updated: 2023/07/22 11:21:55 by edi-iori         ###   ########lyon.fr   */
+/*   Updated: 2023/07/22 13:10:09 by edi-iori         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_H
 # define FT_H
 
-typedef struct t_dictionary
+typedef struct s_dictionary
 {
 	unsigned int number;
 	char *value; 
-} s_dictionary;
+} t_dictionary;
 
 
 void				ft_putstr(char *str);
