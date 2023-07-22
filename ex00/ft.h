11b4d94b65e8c0b821b4d03038f1a9dde280d159
Dorian Eydoux <deydoux@student.42lyon.fr>
2023-07-22 13:18:24 +0200

    Fix ft.h norm

--- ex00/ft.h
+++ ex00/ft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft.h                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: edi-iori <edi-iori@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/22 09:22:51 by deydoux           #+#    #+#             */
-/*   Updated: 2023/07/22 13:10:09 by edi-iori         ###   ########lyon.fr   */
+/*   Updated: 2023/07/22 13:18:02 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 
 typedef struct s_dictionary
 {
-	unsigned int number;
-	char *value; 
-} t_dictionary;
+	unsigned int	number;
+	char			*value;
+}	t_dictionary;
 
 
 void				ft_putstr(char *str);
