4d7cb30a6c6ffabffabba0b344fdd8d19bad79a7
Dorian Eydoux <deydoux@student.42lyon.fr>
2023-07-22 11:31:42 +0200

    Implement error on args parsing

--- ex00/ft_atoi.c
+++ ex00/ft_atoi.c
@@ -6,9 +6,11 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/09 18:27:41 by deydoux           #+#    #+#             */
-/*   Updated: 2023/07/22 11:07:28 by deydoux          ###   ########.fr       */
+/*   Updated: 2023/07/22 11:24:23 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdlib.h>
 
 int	ft_isspace(char c)
 {
@@ -22,15 +24,19 @@ int	ft_isspace(char c)
 
 unsigned int	ft_atoi(char *str)
 {
-	int				i;
+	unsigned int	i;
 	unsigned int	nbr;
 
 	i = 0;
 	nbr = 0;
 	while (ft_isspace(str[i]))
 		i++;
-	while (str[i] == '+')
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			return (NULL);
 		i++;
+	}
 	while ('0' <= str[i] && str[i] <= '9')
 	{
 		nbr = nbr * 10 + str[i] - '0';
--- ex00/rush-02.c
+++ ex00/rush-02.c
@@ -6,11 +6,28 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/22 08:49:53 by deydoux           #+#    #+#             */
-/*   Updated: 2023/07/22 08:50:58 by deydoux          ###   ########.fr       */
+/*   Updated: 2023/07/22 11:30:41 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft.h"
+
 int	main(int argc, char **argv)
 {
+	int		value;
+	char	*dict_file;
 
+	dict_file = "numbers.dict";
+	if (argc == 2)
+		value = ft_atoi(argv[1]);
+	else if (argc == 3)
+	{
+		dict_file = argv[1];
+		value = ft_atoi(argv[2]);
+	}
+	if (!value)
+	{
+		ft_putstr("Error\n");
+		return (2);
+	}
 }
