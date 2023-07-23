c8ecb57799e6836a1743c09efd94200af662b2cf
UnelDev, Enee Di-iorio <enee.prog@gmail.com>
2023-07-23 16:10:23 +0200

    fix bugs in print number

--- ex00/ft.h
+++ ex00/ft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft.h                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
+/*   By: edi-iori <edi-iori@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/22 09:22:51 by deydoux           #+#    #+#             */
-/*   Updated: 2023/07/23 15:09:14 by deydoux          ###   ########.fr       */
+/*   Updated: 2023/07/23 15:59:16 by edi-iori         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,19 @@
 
 typedef struct s_dict
 {
-	long long	value;
-	char		*str;
+	unsigned long long	value;
+	char				*str;
 }	t_dict;
 
-void		ft_putstr(char *str);
-long long	ft_atoi(char *str);
-char		*ft_strjoin(int size, char **strs, char *sep);
-char		**ft_split(char *str, char *charset);
-
-char		*read_file(char *filename);
-t_dict		*parse_dict(char *filename, int *size);
-void		print_number(long long num, t_dict *dict, int dict_len);
+void				ft_putstr(char *str);
+long long			ft_atoi(char *str);
+char				*ft_strjoin(int size, char **strs, char *sep);
+char				**ft_split(char *str, char *charset);
+unsigned long long	ft_iterative_power(unsigned long long nb,
+						unsigned long long power);
+char				*read_file(char *filename);
+t_dict				*parse_dict(char *filename, int *size);
+void				print_number(unsigned long long num, t_dict *dict,
+						int dict_len);
 
 #endif
--- ex00/print_number.c
+++ ex00/print_number.c
@@ -3,94 +3,103 @@
 /*                                                        :::      ::::::::   */
 /*   print_number.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
+/*   By: edi-iori <edi-iori@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/22 14:11:33 by edi-iori          #+#    #+#             */
-/*   Updated: 2023/07/23 14:32:45 by deydoux          ###   ########.fr       */
+/*   Updated: 2023/07/23 16:09:15 by edi-iori         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft.h"
+#include <stdio.h>
+#include <unistd.h>
 
-
-int print_obvious(int num, t_dict *dict, int dic_len)
+void	print_obvious(unsigned long long num, t_dict *dict, int dic_len)
 {
 	int	i;
 
 	i = 0;
 	while (num != dict[i].value && i < dic_len)
 		i++;
-	if (num != dict[i].value)
-	{
-		return (0);
-	}
 	ft_putstr(dict[i].str);
-	return(1);
 }
-unsigned int	intlen(unsigned int n) {
+
+unsigned long long	intlen(unsigned long long n)
+{
 	if (n < 0)
 		return (1 + intlen(-n));
 	else if (n < 10)
 		return (1);
 	else
-		return (1 + intlen(n/10));
+		return (1 + intlen(n / 10));
 }
 
-void print_simple_number(unsigned int num, t_dict *dict, int dict_len)
+void	print_simple_number(unsigned long long num, t_dict *dict, int dict_len)
 {
 	if (num <= 20)
 	{
 		print_obvious(num, dict, dict_len);
 	}
-	else if ( num <= 99)
+	else if (num <= 99)
 	{
 		print_obvious((num / 10) * 10, dict, dict_len);
-		ft_putstr(" ");
-				if((num % 10) != 0)
+		if ((num % 10) != 0)
+		{
+			ft_putstr(" ");
 			print_simple_number(num % 10, dict, dict_len);
-	}else if (num <= 999)
+		}
+	}
+	else if (num <= 999)
 	{
 		print_obvious((num / 100), dict, dict_len);
 		ft_putstr(" ");
 		print_obvious(100, dict, dict_len);
-		ft_putstr(" ");
-		if((num % 100) != 0)
+		if ((num % 100) != 0)
+		{
+			ft_putstr(" ");
 			print_simple_number(num % 100, dict, dict_len);
+		}
 	}
-
 }
-int	ft_iterative_power(int nb, int power)
+
+int	too_big(unsigned long long num, t_dict *dict, int dict_len)
 {
 	int	i;
-	int	total;
 
-	total = 1;
 	i = 0;
-	if (power < 0)
-		return (0);
-	while (i++ < power)
+	while (i < dict_len)
 	{
-		total *= nb;
+		if (dict[i].value >= num)
+			return (0);
+		i++;
 	}
-	return (total);
+	return (1);
 }
 
-void print_number(unsigned int num, t_dict *dict, int dict_len)
+void	print_number(unsigned long long num, t_dict *dict, int dict_len)
 {
-	int length;
+	unsigned long long	length;
 
 	length = ft_iterative_power(10, (intlen(num) - 1) - (intlen(num) - 1) % 3);
+	if (too_big(length, dict, dict_len))
+	{
+		ft_putstr("Dict Error\n");
+		return ;
+	}
 	print_simple_number(num / length, dict, dict_len);
-	ft_putstr(" ");
-	print_obvious(length, dict, dict_len);
-	if (num %length > 999)
+	if (length > 99)
+	{
+		ft_putstr(" ");
+		print_obvious(length, dict, dict_len);
+	}
+	if (num % length > 999)
 	{
 		ft_putstr(" ");
 		print_number(num % length, dict, dict_len);
 	}
-	else if (num %length > 0)
+	else if (num % length > 0)
 	{
 		ft_putstr(" ");
-		print_simple_number(num %length, dict, dict_len);
+		print_simple_number(num % length, dict, dict_len);
 	}
 }
